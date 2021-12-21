559398695a207df544595517df9d74320012a31d
Joshua Huston <joshua.david.huston@gmail.com>
2021-12-21 01:36:50 -0500

    Completed 95% of Linked List implementation Code Challenge 06

--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,13 +1,11 @@
-
-
-
 # Exception Error Handler
 def handle_exceptions(method):
     def wrapper(*args, **kwargs):
         try:
             method(*args, **kwargs)
         except TypeError:
-            print(f'You received an error on {method.__name__} method.')
+            print(f"You received an error on {method.__name__} method.")
+
     return wrapper
 
 
@@ -15,6 +13,7 @@ class Node:
     """
     Create a Node Class
     """
+
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
@@ -25,13 +24,12 @@ class LinkedList:
     Create a Linked List Class
     """
 
-    def __init__(self, head=None, next=None):
+    def __init__(self, head=None, next=None, tail=None):
         # initialization here
         self.head = head
         self.next = next
-        pass
 
-# Insert a new Node into the LinkedList
+    # Insert a new Node into the LinkedList
     @handle_exceptions
     def insert(self, value):
         # method body here
@@ -40,8 +38,7 @@ class LinkedList:
             new_node.next = self.head
         self.head = new_node
 
-
-# Check if a value exists in the Linked List and return a Boolean
+    # Check if a value exists in the Linked List and return a Boolean
     def includes(self, included_value):
         # Return if the value exists as a Node value in the linked list (Boolean). Utilize a while loop to traverse the LL?
         current = self.head
@@ -52,37 +49,95 @@ class LinkedList:
 
         return False
 
-# Find all values in the Linked List and print them in a string
+    # Find all values in the Linked List and print them in a string
     def to_string(self):
         current = self.head
-        output_string = ''
+        output_string = ""
         while current is not None:
-            output_string += f'{current.value} -> '
+            output_string += f"{current.value} -> "
             current = current.next
         else:
-            output_string += 'NULL'
+            output_string += "NULL"
         return output_string
+
+    # Add a new Node to the end (Tail) of the LinkedList
+    def append(self, end_value):
+        new_end_node = Node(end_value)
+        current = self.head
+        while current:
+            if current.next == None:
+                current.next = new_end_node
+                break
+            current = current.next
+
+    # Adds a new Node and value right BEFORE the Node specified
+    def insert_before(self, old_value, new_value):
+        new_node = Node(new_value)
+        current = self.head
+        # new_next = current.next
+        """if current.next == None:
+            print(f"Current Value= {current.value}")
+            print(f"NewNode.next = {new_node.next}")
+            print(f"NewNode= {new_node.value}")
+            new_node.next = current.value
+            current.next = current.value
+            current.value = new_node.value
+
+            print(f"current.value= {current.value}")
+        print(f"current.next= {current.next}")
+        print(f"newnode.value= {new_node.value}")
+        print(f"newnode.next= {new_node.next}")
+        print(f"current= {current.value}")"""
+
+        while current.next is not None:
+            new_next = current.next
+            print(f"New Next.value = {new_next.value}")
+            if new_next.value == old_value:
+                new_node.next = current.next
+                current.next = new_node
+                break
+            current = current.next
+
+    # Adds a new Node and value right AFTER the Node specified
+    def insert_after(self, old_value, new_value):
+        new_node = Node(new_value)
+        current = self.head
+        while current is not None:
+            if current.value == old_value:
+                new_node.next = current.next
+                current.next = new_node
+            current = current.next
 
 
 if __name__ == "__main__":
 
     test = LinkedList()
+    test2 = LinkedList()
 
-    test.insert('pear')
-    test.insert('raspberry')
-    test.insert('kiwi')
-    test.insert('apple')
-    print(test.includes('kiwi'))
-    print(test.includes('apricot'))
+    """test.insert("pear")
+    test.insert("raspberry")
+    test.insert("kiwi")
+    test.insert("apple")
+    test.append("pineapple")
+    test.append("blueberry")
+    test.insert_after("kiwi", "peach")
+    test.insert_before("pear", "papaya")
+
+    print(test.includes("kiwi"))
+    print(test.includes("apricot"))
     # test_output = test.to_string()
-    print(f'Head: {test.head.value}')
-    print(f'All Nodes: {test.to_string()}')
+    print(f"Head: {test.head.value}")
+    print(f"All Nodes: {test.to_string()}")
+"""
+    test2.insert("apple")
+    test2.insert_before("apple", "peach")
+    print(f"Head test 2: {test2.head.value}")
+    print(f"All Nodes Test 2: {test2.to_string()}")
 
     # Test error handling
     test.insert()
 
 # print(test_output)
-
 
 
 """
@@ -93,4 +148,3 @@ if __name__ == '__main__':
     ll.insert(6)
     ll.insert(3)
     ll.__str__()"""
-
--- python/tests/test_linked_list.py
+++ python/tests/test_linked_list.py
@@ -2,6 +2,7 @@ from linked_list.linked_list import LinkedList, Node
 
 import pytest
 
+
 def test_import():
     assert LinkedList
 
@@ -28,52 +29,125 @@ def test_linked_list_empty_insert():
     ll = LinkedList()
     assert ll.head == None
 
+
 # Test to check if insert() function will work with an empty Linked List
 def test_insert_to_empty_linked_list():
     # ll.head = apple
     ll = LinkedList()
-    ll.insert('apple')
-    assert ll.head.value == 'apple'
+    ll.insert("apple")
+    assert ll.head.value == "apple"
+
 
 # Test to check if insert() function will work with a Linked List that has a Head stored.
 def test_insert_to_linked_list():
     ll = LinkedList()
-    node1 = Node('apple')
+    node1 = Node("apple")
     ll.head == node1
-    node2 = Node('pear')
+    node2 = Node("pear")
     node1.next == node2
-    ll.insert('banana')
-    assert ll.head.value == 'banana'
+    ll.insert("banana")
+    assert ll.head.value == "banana"
+
 
 # Test to check if the LinkedList includes() function works when a value is stored in a node
 def test_includes_in_linked_list():
     ll = LinkedList()
-    node1 = ll.insert('apple')
+    node1 = ll.insert("apple")
     ll.head == node1
-    node2 = ll.insert('pear')
-    test_value = ll.includes('pear')
+    node2 = ll.insert("pear")
+    test_value = ll.includes("pear")
     assert test_value == True
+
 
 # Test to check if the includes() function works when a value is NOT stored in a node.
 def test_not_includes_in_linked_list():
     ll = LinkedList()
-    node1 = ll.insert('apple')
+    node1 = ll.insert("apple")
     ll.head == node1
-    node2 = ll.insert('banana')
-    test_value = ll.includes('pear')
+    node2 = ll.insert("banana")
+    test_value = ll.includes("pear")
     assert test_value == False
+
 
 # Test to check if to_string function is printing the LinkedList output correctly
 def test_to_string():
     ll = LinkedList()
-    node1 = ll.insert('apple')
+    node1 = ll.insert("apple")
     ll.head == node1
-    node2 = ll.insert('banana')
+    node2 = ll.insert("banana")
     test_output = ll.to_string()
     assert test_output == "banana -> apple -> NULL"
+
 
 # Test to see if to_string will function when passed an empty Linked List
 def test_to_string_empty():
     ll = LinkedList()
     test_output = ll.to_string()
     assert test_output == "NULL"
+
+
+# Test to see if one node can be appended to the end of a Linked List
+def test_append():
+    ll = LinkedList()
+    node1 = ll.insert("apple")
+    ll.head == node1
+    node2 = ll.insert("banana")
+    ll.append("peach")
+    test_output = ll.to_string()
+    assert test_output == "banana -> apple -> peach -> NULL"
+
+
+# Test to see if multiple nodes can be appended to the end of a Linked List
+def test_append_multiple():
+    ll = LinkedList()
+    node1 = ll.insert("apple")
+    ll.head == node1
+    ll.append("peach")
+    ll.append("pineapple")
+    expected = ll.to_string()
+    assert expected == "apple -> peach -> pineapple -> NULL"
+
+
+# Test if you can insert a node before a node in the middle of a Linked List
+def test_insert_mid_before():
+    ll = LinkedList()
+    node1 = ll.insert("apple")
+    ll.head == node1
+    ll.insert("kiwi")
+    ll.insert("blueberry")
+    ll.insert_before("kiwi", "peach")
+    expected = ll.to_string()
+    assert expected == "blueberry -> peach -> kiwi -> apple -> NULL"
+
+
+# Test if you can insert a node before the first node of a Linked List
+"""def test_insert_beg():
+    ll = LinkedList()
+    node1 = ll.insert("apple")
+    ll.head == node1
+    ll.insert_before("apple", "peach")
+    expected = ll.to_string()
+    assert expected == "peach -> apple -> NULL"
+"""
+
+# Test if you can insert a node after the middle node of a Linked List
+def test_insert_mid_after():
+    ll = LinkedList()
+    node1 = ll.insert("apple")
+    ll.head == node1
+    ll.insert("kiwi")
+    ll.insert("blueberry")
+    ll.insert_after("kiwi", "peach")
+    expected = ll.to_string()
+    assert expected == "blueberry -> kiwi -> peach -> apple -> NULL"
+
+
+# Test if you can insert a node after the last node of a linked list
+def test_insert_last_after():
+    ll = LinkedList()
+    node1 = ll.insert("apple")
+    ll.head == node1
+    ll.insert("kiwi")
+    ll.insert_after("apple", "peach")
+    expected = ll.to_string()
+    assert expected == "kiwi -> apple -> peach -> NULL"
